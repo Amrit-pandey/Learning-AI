0dd4d91b1053d30f6d4ee86c4f1d7ec44bc32a11
ampandey <amritpandey2005@gmail.com>
2026-07-02 10:13:34 +0530

    added streaming in Gemini response

--- project02-sales-brochure-generator/utils/gemini.py
+++ project02-sales-brochure-generator/utils/gemini.py
@@ -23,8 +23,15 @@ def generate_brochure(client: genai.Client, website: Fetch_website):
     Website Content:
     {website.text}
     """
-    response = client.models.generate_content(
+    response = client.models.generate_content_stream(
         model="gemini-2.5-flash",
         contents= prompt
     )
-    return response.text
+    final_text = ""
+
+    for chunk in response:
+        if chunk.text:
+            print(chunk.text, end="", flush=True)
+            final_text += chunk.text
+
+    return final_text
